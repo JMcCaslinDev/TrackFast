aed1a592796749bbdbb5961f0db0e466d82ca94d
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-07-02 19:02:04 -0700

    comment out console log for job entries

--- app/mongodb/api/getJobApplications.js
+++ app/mongodb/api/getJobApplications.js
@@ -2,13 +2,6 @@
 
 import JobApplication from '../jobApplicationModel';
 
-// Utility function to truncate strings
-function truncateString(str, num) {
-  if (str.length <= num) {
-    return str;
-  }
-  return str.slice(0, num) + '...';
-}
 
 // Utility function to convert to plain object
 function toPlainObject(obj) {
@@ -30,14 +23,11 @@ export async function getJobApplications(userId) {
         plainJob.date_applied = new Date(plainJob.date_applied).toISOString();
       }
 
-      if (plainJob.job_description) {
-        plainJob.job_description = truncateString(plainJob.job_description, 100); // Adjust the length as needed
-      }
 
       return plainJob;
     });
 
-    console.log('Job applications:', processedApplications);
+    // console.log('Job applications:', processedApplications);
     return processedApplications;
   } catch (error) {
     console.error('Error fetching job applications:', error);
